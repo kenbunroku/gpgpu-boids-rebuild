5937bf4e018c2f1926415db1bc2533f49e4b50a1
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2024-06-03 05:44:59 -0500

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import glsl from "vite-plugin-glsl";
 export default defineConfig(() => {
   return {
     root: "src",
-    // publicDir: "../public",
-    // base: "/sakura-rebuild/",
-    // build: {
-    //   outDir: "../dist",
-    //   emptyOutDir: true,
-    // },
+    publicDir: "../public",
+    base: "/gpgpu-boids-rebuild/",
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+    },
     plugins: [glsl()],
   };
 });
